a89af0cd15eedf60624c694ed8ef9158f242e7ed
dlugs <sdlugosz@nethead.pl>
2017-10-08 22:03:33 +0200

    emit event

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('mymessage', function(msg){
-    console.log(msg);
+    io.emit('mymessage', msg);
   });
 });
 
